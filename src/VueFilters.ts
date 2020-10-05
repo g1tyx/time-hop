2498097f6437564bda5482d282f79996a613e619
Isha Dijcks <ishadijcks@gmail.com>
2020-10-05 22:01:15 +0200

    1000s comma separator

--- src/VueFilters.ts
+++ src/VueFilters.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 
 Vue.filter('twoDigits', function (value: number) {
-    return value.toFixed(2)
+    return value.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 })
